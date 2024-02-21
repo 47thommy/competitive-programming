a22dffa337ec8e9d002a6112144480cea8b38445
Thomas Wondwosen <113582664+47thommy@users.noreply.github.com>
2024-02-21 17:40:27 +0300

    Add solution for Partition List

---
+++ python_track/leetcode/partition-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        front = dummy
+        dummy2 = ListNode(0)
+        back = dummy2
+        while head != None :
+            if head.val < x :
+                front.next = head
+                front = front.next
+            else :
+                back.next = head
+                back = back.next
+            
+            head = head.next
+        
+        back.next = None
+        front.next = dummy2.next
+        return dummy.next
