e57b5f9e3ad821d50d54c978df1f5da9bab4fa3f
Thomas Wondwosen <113582664+47thommy@users.noreply.github.com>
2024-03-02 11:21:18 +0300

    Add solution for K-th Symbol in Grammar

---
+++ python_track/leetcode/k-th-symbol-in-grammar.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        ans = 0
+        l=1
+        r= 2 ** (n - 1)
+        while l < r:
+            m = (l + r) // 2
+            if k <= m:
+                r = m 
+            else:
+                l = m + 1
+                ans = 0 if ans == 1 else 1
+        return ans
