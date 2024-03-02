176805f1bd3377abdbc0e8644de77fccccdd7c7e
Thomas Wondwosen <113582664+47thommy@users.noreply.github.com>
2024-03-02 13:05:30 +0300

    Add solution for N-Queens

---
+++ python_track/leetcode/n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        row=set()
+        col=set()
+        mainD=set()
+        antiD=set()
+        res=[]
+        board=[["."] * n for i in range(n)]
+        print(board)
+        def backtrack(r):
+            if r == n:
+                ans= ["".join(row) for row in board]
+                res.append(ans)
+                return
+            for c in range(n):
+                if c in col or r+c in mainD or r-c in antiD:
+                    continue
+                col.add(c)
+                mainD.add(r+c)
+                antiD.add(r-c)
+                board[r][c] = "Q"
+                backtrack(r+1)
+                col.remove(c)
+                mainD.remove(r+c)
+                antiD.remove(r-c)
+                board[r][c]="."
+        backtrack(0)
+        return res
